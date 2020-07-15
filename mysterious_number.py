c6021cda24bdf24ca9cdcc860b7b984f9abe8546
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-15 07:26:49 +0200

    nb myst ok mais à améliorer

--- mysterious_number.py
+++ mysterious_number.py
@@ -10,40 +10,39 @@ round_left = 3
 while number_test_left != 0 :
     if round_left != 0 and number_test_left != 0 or player_number != sphinx_number :
         sphinx_number = random.randint(1,100)
-        player_number = int(input("\nTapes un nombre entre 1 et 100 :  ")) 
+        player_number = int(input("\n--> Tapes un nombre entre 1 et 100 :  ")) 
         print(sphinx_number)
-        while player_number != sphinx_number :
+        while player_number != sphinx_number and number_test_left != 0 and round_left != 0:
 
             if player_number < sphinx_number :
-                print("\nLe nombre que j'ai en tête est plus \u001b[1mgrand\u001b[0m.\n")
+                print("\n'Le nombre que j'ai en tête est plus \u001b[1mgrand\u001b[0m.'")
                 number_test_left -= 1
-                print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
-                
-                player_number = int(input("\nTapes un nombre entre 1 et 100 :  "))  
+                if number_test_left != 0 :
+                    print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
+                    player_number = int(input("\n--> Tapes un nombre entre 1 et 100 :  "))  
+                else :
+                    print(f'\nTu as utilisé tes 20 essais sans succès.\n Retentes ta chance. ')
 
             elif player_number > sphinx_number : 
-                print("\nLe nombre que j'ai en tête est plus \u001b[1mpetit\u001b[0m.\n")
-                print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
+                print("\nLe nombre que j'ai en tête est plus \u001b[1mpetit\u001b[0m.")
                 number_test_left -= 1
-                player_number = int(input("\nTapes un nombre entre 1 et 100 :  ")) 
-                
-            
-        print(f'\nBien joué ! Le nombre mystérieux est : {sphinx_number}.\n')
-        number_test_left -= 1
-        round_left -= 1
-        if round_left != 0 :
-            print(f'\nTu as encore {round_left} nombre(s) mystérieu(x) à trouver.\n' )
-            print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
-        else:
-            print(f'Bravo tu as trouvé les 3 nombres mystérieux en {20-number_test_left} essais !')
-    print(f'Tu as utilisé tes {number_test_left} essais sans succès.\n Retentes ta chance. ')
+                if number_test_left != 0 :
+                    print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
+                    player_number = int(input("\n--> Tapes un nombre entre 1 et 100 :  ")) 
+                else :
+                    print(f'\nTu as utilisé tes 20 essais sans succès.\n Retentes ta chance. ')
+        if player_number == sphinx_number :
+            print(f'\nBien joué ! Le nombre mystérieux est : {sphinx_number}.\n')
+            number_test_left -= 1
+            round_left -= 1
+            if round_left != 0 :
+                print(f'\nTu as encore {round_left} nombre(s) mystérieu(x) à trouver.\n' )
+                print(f'Il te reste encore {number_test_left} essais pour trouver le(s) {round_left} nombre(s) mystérieu(x) restant(s).\n')
+            elif round_left == 0 :
+                print(f'!! Bravo !! Tu as trouvé les 3 nombres mystérieux en {20-number_test_left} essais !\nTu peux prendre la clé de bronze !')
+                number_test_left = 0
+
+# !!!!! Mettre la clé dans le sac à dos !!!!
 
 
 
-
-# try_again_or_not()
-
-
-
-
-print("tu as le droit à encore {3} essais")
